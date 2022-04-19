f70a59d731c1dcd80186408b07b8c5a2c15b6b02
oburnett127 <oburnett127@gmail.com>
2022-04-19 14:13:36 -0500

    Added eureka annotation and added spring cloud dependency. Fixed dependency versions.

--- transaction-service/src/main/java/com/oburnett127/transactionservice/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/oburnett127/transactionservice/TransactionServiceApplication.java
@@ -2,6 +2,7 @@ package com.oburnett127.transactionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
